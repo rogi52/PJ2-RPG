9a8ebd0c879b58e638149c9bda4873c4493e90b0
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-07-11 13:54:58 +0900

    Create Moji.java

---
+++ src/Moji.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Moji {
+
+	public static void main(String[] args) {
+
+	}
+
+	// arrange の逆変換が必要になる？
+	static String mark1  = "がぎぐげごガギグゲゴざじずぜぞザジズゼゾだぢづでどダヂヅデドばびぶべぼバビブベボ";
+	static String trans1 = "かきくけこカキクケコさしすせそサシスセソたちつてとタチツテトはひふへほハヒフヘホ";
+	static String mark2  = "ぱぴぷぺぽパピプペポ";
+	static String trans2 = "はひふへほハヒフヘホ";
+	static String arrange(String cmd) {
+		String res = "";
+		for(int i = 0, pos = -1; i < cmd.length(); i++) {
+			char c = cmd.charAt(i);
+			if((pos = mark1.indexOf(c)) != -1)
+				res += trans1.charAt(pos) + "゛";
+			else if((pos = mark2.indexOf(c)) != -1)
+				res += trans2.charAt(pos) + "゜";
+			else
+				res += c;
+		}
+		return res;
+	}
+
+	static BufferedImage getImage(char c) throws IOException {
+		BufferedImage I = ImageIO.read(new File("./文字/" + c + ".png"));
+		return I;
+	}
+}
