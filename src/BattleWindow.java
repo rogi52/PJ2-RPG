e4ca651f02d0d9fdb07d7191380e42389479c61c
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-07-16 17:00:27 +0900

    Battle.java と連動するようにした

--- src/BattleWindow.java
+++ src/BattleWindow.java
@@ -11,73 +11,17 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
-
 public class BattleWindow extends Component implements KeyListener {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setSize(960, 640);
+	final int NULL    = 0;
+	final int MESSAGE = 1;
+	final int COMMAND = 2;
+	int STATE = NULL;
 
-		Player[] players = new Player[4];
-		players[0] = new Player("のびた", 99, 100);
-		players[1] = new Player("しずか", 23, 45);
-		players[2] = new Player("すねすね", 10, 0);
-		players[3] = new Player("ジャイ", 555, 5);
-
-		ArrayList<String> cmds1 = new ArrayList<>();
-		cmds1.add("こうげき");
-		cmds1.add("スキル");
-		cmds1.add("にげる");
-
-		ArrayList<ArrayList<String>> cmds2 = new ArrayList<>();
-		for(int i = 0; i < 3; i++) {
-			ArrayList<String> cmds = new ArrayList<>();
-			for(char c = 'A'; c <= 'Z'; c++) {
-				cmds.add(cmds1.get(i) + c);
-			}
-			cmds2.add(cmds);
-		}
-
-		ArrayList<String> cmds3 = new ArrayList<>();
-		cmds3.add("AAAA");
-		cmds3.add("BBBB");
-		cmds3.add("CCCC");
-		cmds3.add("DDDD");
-
-		BattleWindow bw = new BattleWindow(players, cmds1, cmds2, cmds3);
-		frame.add(bw);
-		frame.addKeyListener(bw);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-
-		String res = bw.getOption();
-		System.out.println(res);
-
-		res = bw.getOption();
-		System.out.println(res);
-
-		res = bw.getOption();
-		System.out.println(res);
-
-		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-
-		bw.println("AAAAに3のダメージ", true);
-
-		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-
-		bw.println("おれのターン", false);
-
-		try { Thread.sleep(3000); } catch (InterruptedException e) {}
-
-		bw.println("あたらしいやつ", true);
-	}
-
-	final int LARGE_SELECT = 0;
-	final int SMALL_SELECT = 1;
-	final int ENEMY_SELECT = 2;
-	final int FINISH = 3;
-	int PHASE = LARGE_SELECT;
+	public static final int CMD_LEFT       = 3; // 選択肢は4つまで
+	public static final int CMD_RIGHT      = 4; // 選択肢は4つまで
+	public static final int CMD_RIGHT_BOX6 = 5;
+	int CMD_TYPE = NULL;
 
 	static class Player {
 		String name = "";
@@ -92,7 +36,57 @@ public class BattleWindow extends Component implements KeyListener {
 	/* busy waiting? */
 	ByteArrayOutputStream bos = new ByteArrayOutputStream();
 	PrintWriter out = new PrintWriter(bos);
-	String getOption() {
+
+	Player[] players = new Player[4];
+	BattleWindow() {}
+	BattleWindow(Player[] players) {
+		this.players = players;
+		for(int i = 0; i < 4; i++) players[i].name = ImageManager.arrange(players[i].name);
+	}
+
+	int posLeft = 0;
+	ArrayList<String> cmdsLeft = new ArrayList<>();
+	ArrayList<String[][]> cmdsBox6 = new ArrayList<>();
+
+
+	String getOption(ArrayList<String> cmds, int windowType) {
+		STATE = COMMAND;
+		CMD_TYPE = windowType;
+		switch(CMD_TYPE) {
+			case CMD_LEFT: {
+				cmdsLeft.clear();
+				for(int i = 0; i < cmds.size(); i++) cmdsLeft.add(ImageManager.arrange(cmds.get(i)));
+			} break;
+
+			case CMD_RIGHT: {
+				cmdsBox6.clear();
+				String[][] box = new String[4][];
+				for(int i = 0; i < 4; i++) {
+					box[i] = new String[2];
+					if(i < cmds.size()) box[i][0] = ImageManager.arrange(cmds.get(i));
+				}
+				cmdsBox6.add(box);
+			} break;
+
+			case CMD_RIGHT_BOX6: {
+				cmdsBox6.clear();
+				for(int i = 0; i < cmds.size(); i += 6) {
+					String[][] box = new String[4][];
+					for(int j = 0; j < 4; j++) box[j] = new String[2];
+					for(int j = 0; j < 6; j++) if(i + j  < cmds.size()) box[j % 3][j / 3] = cmds.get(i + j);
+					if(i > 0) box[3][0] = "LEFT";
+					if(i + 6 < cmds.size()) box[3][1] = "RIGHT";
+					cmdsBox6.add(box);
+				}
+			} break;
+
+			default: {
+				System.out.println("ERROR windowType : " + windowType);
+			} break;
+		}
+
+		repaint();
+
 		try {
 			while(true) {
 				ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
@@ -101,45 +95,58 @@ public class BattleWindow extends Component implements KeyListener {
 				if(res != null) {
 					bos = new ByteArrayOutputStream();
 					out = new PrintWriter(bos);
+					STATE = NULL;
 					return res;
+					/* コマンド用のクラス = [name, hash] を作るといいかも */
 				}
 			}
 		} catch (IOException e) {}
+
+		STATE = NULL;
+		CMD_TYPE = NULL;
 		return "ERROR";
 	}
 
-	Player[] players = new Player[4];
-	BattleWindow(Player[] players, ArrayList<String> cmds1, ArrayList<ArrayList<String>> cmds2, ArrayList<String> cmds3) {
-		this.players = players;
-		for(int i = 0; i < 4; i++) players[i].name = ImageManager.arrange(players[i].name);
-		for(int i = 0; i < cmds1.size(); i++) cmds1.set(i, ImageManager.arrange(cmds1.get(i)));
-		for(int i = 0; i < cmds2.size(); i++)
-			for(int j = 0; j < cmds2.get(i).size(); j++)
-				cmds2.get(i).set(j, ImageManager.arrange(cmds2.get(i).get(j)));
-		for(int i = 0; i < cmds3.size(); i++) cmds3.set(i, ImageManager.arrange(cmds3.get(i)));
-		this.cmds1 = cmds1;
-		this.cmds2 = cmds2;
-		this.cmds3 = cmds3;
+	int cmdArrayPos = 0;
+	int posRightI = 0, posRightJ = 0;
+
+	MessageWindow mw = new MessageWindow("", 352, 448, 32, 4, 15);
+	void println(String str, int newWindow, int waitEnterKey) {
+		print(str + '\n', newWindow, waitEnterKey);
 	}
 
-	int selectPos1 = 0;
-	ArrayList<String> cmds1 = new ArrayList<>();
-	ArrayList<ArrayList<String>> cmds2 = new ArrayList<>();
-	ArrayList<String[][]> cmds12 = new ArrayList<>();
-	ArrayList<String> cmds3 = new ArrayList<>();
+	public final static int NEW_WINDOW = 0;
+	public final static int WAIT_ENTER_KEY = 1;
+	public final static int CONTINUE = 2;
 
-	int cmdArrayPos = 0;
-	int selectPos2I = 0, selectPos2J = 0;
-
-	MessageWindow mw = null;
-	void println(String str, boolean newWindow) {
-		str = ImageManager.arrange(str) + '\n';
-		if(newWindow || mw == null) mw = new MessageWindow("", 352, 448, 32, 4, 15);
+	void print(String str, int newWindow, int waitEnterKey) {
+		str = ImageManager.arrange(str);
+		STATE = MESSAGE;
+		if(newWindow == NEW_WINDOW) mw.buffer = "";
 		for(int i = 0; i < str.length(); i++) {
 			mw.addMessageStatic("" + str.charAt(i), false);
 			repaint();
 			try { Thread.sleep(50); } catch (InterruptedException e) {}
 		}
+
+		if(waitEnterKey == WAIT_ENTER_KEY) {
+			try {
+				while(true) {
+					ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+					BufferedReader in = new BufferedReader(new InputStreamReader(bis));
+					String signal = in.readLine();
+					if(signal != null) {
+						bos = new ByteArrayOutputStream();
+						out = new PrintWriter(bos);
+						break;
+					}
+				}
+			} catch (IOException e) {}
+		}
+
+		/* 途中で ENTER が押された場合 */
+
+		STATE = NULL;
 	}
 
 	int startX[] = {128, 320, 512, 704};
@@ -176,7 +183,7 @@ public class BattleWindow extends Component implements KeyListener {
 			try {
 				BufferedImage image = ImageManager.getCharImage('M');
 				g.drawImage(image, startX[i], 128, null);
-				String num = players[i].HP.toString();
+				String num = players[i].MP.toString();
 				while(num.length() < 3) num = ' ' + num;
 				for(int j = 0; j < 3; j++) {
 					if(num.charAt(j) != ' ') {
@@ -187,141 +194,124 @@ public class BattleWindow extends Component implements KeyListener {
 			} catch (IOException e) { System.out.println("Error : MP"); }
 		}
 
-		if(mw != null) mw.paint(g);
-
-		if(PHASE == FINISH) return;
-
-		/* コマンド(左) */
-		try {
-			BufferedImage arrow = ImageManager.getImage("arrow");
-			g.drawImage(arrow, 128 - 32, 448 + selectPos1 * 32, null);
-		} catch (IOException e) { System.out.println("Error : arrow"); }
-
-		for(int i = 0; i < cmds1.size(); i++) {
-			for(int j = 0; j < cmds1.get(i).length(); j++) {
-				try {
-					BufferedImage image = ImageManager.getCharImage(cmds1.get(i).charAt(j));
-					g.drawImage(image, 128 - 32 + (j + 1) * 32, 448 + i * 32, null);
-				} catch (IOException e) { System.out.println("Error : " + cmds1.get(i).charAt(j)); }
-			}
+		if(STATE == MESSAGE) {
+			mw.paint(g);
 		}
 
-		/* コマンド(右) */
-		if(PHASE == SMALL_SELECT || PHASE == ENEMY_SELECT) {
-			String[][] cmds = cmds12.get(cmdArrayPos);
-			for(int i = 0; i < 4; i++) {
-				for(int j = 0; j < 2; j++) {
-					if(cmds[i][j] == null) continue;
-					for(int k = 0; k < cmds[i][j].length(); k++) {
-						try {
-							BufferedImage image = ImageManager.getCharImage(cmds[i][j].charAt(k));
-							g.drawImage(image, 352 + 32 + j * 32 * 8 + k * 32, 448 + i * 32, null);
-						} catch (IOException e) { System.out.println("Erroe : " + cmds[i][j].charAt(k)); }
-					}
+		if(STATE == COMMAND) {
+			/*  コマンド (左) */
+			try {
+				BufferedImage arrow = ImageManager.getCharImage('▶');
+				g.drawImage(arrow, 128 - 32, 448 + posLeft * 32, null);
+			} catch (IOException e) { System.out.println("Error : arrow"); }
+
+			for(int i = 0; i < cmdsLeft.size(); i++) {
+				for(int j = 0; j < cmdsLeft.get(i).length(); j++) {
+					try {
+						BufferedImage image = ImageManager.getCharImage(cmdsLeft.get(i).charAt(j));
+						g.drawImage(image, 128 - 32 + (j + 1) * 32, 448 + i * 32, null);
+					} catch (IOException e) { System.out.println("Error : " + cmdsLeft.get(i).charAt(j)); }
 				}
 			}
 
-			try {
-				BufferedImage arrow = ImageManager.getImage("arrow");
-				g.drawImage(arrow, 352 + selectPos2J * 32 * 8, 448 + selectPos2I * 32, null);
-			} catch (IOException e) { System.out.println("Error : arrow"); }
+			/* コマンド (右) */
+			if(CMD_TYPE == CMD_RIGHT || CMD_TYPE == CMD_RIGHT_BOX6) {
+				String[][] box = cmdsBox6.get(cmdArrayPos);
+				for(int i = 0; i < 4; i++) {
+					for(int j = 0; j < 2; j++) {
+						if(box[i][j] == null) continue;
+						for(int k = 0; k < box[i][j].length(); k++) {
+							try {
+								BufferedImage image = ImageManager.getCharImage(box[i][j].charAt(k));
+								g.drawImage(image, 352 + 32 + j * 32 * 8 + k * 32, 448 + i * 32, null);
+							} catch (IOException e) { System.out.println("Error : " + box[i][j].charAt(k)); }
+						}
+					}
+				}
+
+				try {
+					BufferedImage arrow = ImageManager.getCharImage('▶');
+					g.drawImage(arrow, 352 + posRightJ * 32 * 8, 448 + posRightI * 32, null);
+				} catch (IOException e) { System.out.println("Error : arrow"); }
+			}
 		}
 	}
-
-	ArrayList<String[][]> transBox6(ArrayList<String> cmd) {
-		ArrayList<String[][]> res = new ArrayList<>();
-		for(int i = 0; i < cmd.size(); i += 6) {
-			String[][] name = new String[4][];
-			for(int j = 0; j < 4; j++) name[j] = new String[2];
-			for(int j = 0; j < 6; j++) if(i + j < cmd.size()) name[j % 3][j / 3] = cmd.get(i + j);
-			if(i != 0) name[3][0] = "LEFT";
-			if(i + 6 < cmd.size()) name[3][1] = "RIGHT";
-			res.add(name);
-		}
-		return res;
-	}
-
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(PHASE == LARGE_SELECT) {
+
+		if(STATE == MESSAGE) {
 			switch(e.getKeyCode()) {
-				case KeyEvent.VK_DOWN: { selectPos1 = Math.min(selectPos1 + 1, cmds1.size() - 1); repaint(); break; }
-				case KeyEvent.VK_UP: { selectPos1 = Math.max(selectPos1 - 1, 0); repaint(); break; }
 				case KeyEvent.VK_ENTER: {
-					PHASE = SMALL_SELECT;
-					out.println(cmds1.get(selectPos1));
+					mw.clear();
+					out.println("ENTER");
 					out.flush();
-					cmds12 = transBox6(cmds2.get(selectPos1));
-					repaint();
-				}
+				} break;
 			}
 		}
-		else if(PHASE == SMALL_SELECT || PHASE == ENEMY_SELECT) {
-			switch(e.getKeyCode()) {
-				case KeyEvent.VK_DOWN: {
-					if(selectPos2I == 3) break;
-					int pI = selectPos2I;
-					while(true) {
-						selectPos2I++; if(selectPos2I == 4) break;
-						if(cmds12.get(cmdArrayPos)[selectPos2I][selectPos2J] != null) break;
-					}
-					if(selectPos2I == 4) { selectPos2I = pI; break; }
-					repaint(); break;
-				}
-				case KeyEvent.VK_UP: {
-					if(selectPos2I == 0) break;
-					int pI = selectPos2I;
-					while(true) {
-						selectPos2I--; if(selectPos2I == -1) break;
-						if(cmds12.get(cmdArrayPos)[selectPos2I][selectPos2J] != null) break;
-					}
-					if(selectPos2I == -1) { selectPos2I = pI; break; }
-					repaint(); break;
-				}
-				case KeyEvent.VK_LEFT: {
-					if(selectPos2J == 0) break;
-					if(cmds12.get(cmdArrayPos)[selectPos2I][selectPos2J - 1] == null) break;
-					selectPos2J--;
-					repaint(); break;
-				}
-				case KeyEvent.VK_RIGHT: {
-					if(selectPos2J == 1) break;
-					if(cmds12.get(cmdArrayPos)[selectPos2I][selectPos2J + 1] == null) break;
-					selectPos2J++;
-					repaint(); break;
-				}
-				case KeyEvent.VK_ENTER: {
-					String cmd = cmds12.get(cmdArrayPos)[selectPos2I][selectPos2J];
-					if(cmd.equals("LEFT")) {
-						cmdArrayPos--;
-						selectPos2I = 0;
-						selectPos2J = 0;
-					} else if(cmd.equals("RIGHT")) {
-						cmdArrayPos++;
-						selectPos2I = 0;
-						selectPos2J = 0;
-					} else {
-						out.println(cmd);
-						out.flush();
-						cmdArrayPos = 0;
-						selectPos2I = 0;
-						selectPos2J = 0;
-						if(PHASE == SMALL_SELECT) {
-							PHASE = ENEMY_SELECT;
-							cmds12.clear();
-							String[][] name = new String[4][];
-							for(int i = 0; i < 4; i++) {
-								name[i] = new String[2];
-								if(i < cmds3.size()) name[i][0] = cmds3.get(i);
-							}
-							cmds12.add(name);
-						} else if(PHASE == ENEMY_SELECT) {
-							PHASE = FINISH;
+
+		if(STATE == COMMAND) {
+			switch(CMD_TYPE) {
+				case CMD_LEFT: {
+					switch(e.getKeyCode()) {
+						case KeyEvent.VK_DOWN:  { posLeft = Math.min(posLeft + 1, cmdsLeft.size() - 1); repaint(); } break;
+						case KeyEvent.VK_UP:    { posLeft = Math.max(posLeft - 1,                   0); repaint(); } break;
+						case KeyEvent.VK_ENTER: {
+							out.println(cmdsLeft.get(posLeft));
+							out.flush();
 						}
 					}
-					repaint();
-				}
+				} break;
+
+				/* case CMD_RIGHT or CMD_RIGHT_BOX6: */
+				default: {
+					switch(e.getKeyCode()) {
+						case KeyEvent.VK_DOWN: {
+							int pI = posRightI;
+							while(true) { posRightI++; if(posRightI == 4) break; if(cmdsBox6.get(cmdArrayPos)[posRightI][posRightJ] != null) break; }
+							if(posRightI == 4) { posRightI = pI; break; }
+							repaint();
+						} break;
+
+						case KeyEvent.VK_UP: {
+							int pI = posRightI;
+							while(true) { posRightI--; if(posRightI == -1) break; if(cmdsBox6.get(cmdArrayPos)[posRightI][posRightJ] != null) break; }
+							if(posRightI == -1) { posRightI = pI; break; }
+							repaint();
+						} break;
+
+						case KeyEvent.VK_LEFT: {
+							if(posRightJ == 0) break;
+							if(cmdsBox6.get(cmdArrayPos)[posRightI][posRightJ - 1] == null) break;
+							posRightJ--;
+							repaint();
+						} break;
+
+						case KeyEvent.VK_RIGHT: {
+							if(posRightJ == 1) break;
+							if(cmdsBox6.get(cmdArrayPos)[posRightI][posRightJ + 1] == null) break;
+							posRightJ++;
+							repaint();
+						} break;
+
+						case KeyEvent.VK_ENTER: {
+							String cmd = cmdsBox6.get(cmdArrayPos)[posRightI][posRightJ];
+							posRightI = 0;
+							posRightJ = 0;
+							if(cmd.equals("LEFT")) {
+								cmdArrayPos--;
+								repaint();
+							} else if(cmd.equals("RIGHT")) {
+								cmdArrayPos++;
+								repaint();
+							} else {
+								cmdArrayPos = 0;
+								out.println(cmd);
+								out.flush();
+							}
+						} break;
+					}
+				} break;
 			}
 		}
 	}
