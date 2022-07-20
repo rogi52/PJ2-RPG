53d30028670de1faa5fd85e80cf7bf1e8b8a1c7f
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-07-20 18:44:45 +0900

    resetKey() の位置の修正

--- src/AnimationThread.java
+++ src/AnimationThread.java
@@ -577,13 +577,13 @@ class AnimationMove extends Thread{
 				int enemyUnitID = enemy_type; //(MobData に定数を記載. MINION, BOSS_1ST_FLOOR など)
 				Battle battle = new Battle(w.m, dungeonID, enemyUnitID, w.myCanvas);
 
-　                             w.resetKey();
 				w.removeKeyListener(w.getKeyListeners()[0]);
 				w.addKeyListener(battle.window);
 
 				int result = battle.start();
 
 				w.removeKeyListener(battle.window);
+				w.resetKey();
 				w.addKeyListener(w);
 
 				System.out.println(result);
