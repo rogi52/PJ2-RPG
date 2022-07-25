6af1933c196060aa91e7739f5e7b5d2c087eb4e0
rogi52 <91960528+rogi52@users.noreply.github.com>
2022-07-25 20:39:51 +0900

    Update Battle.java

--- src/Battle.java
+++ src/Battle.java
@@ -188,7 +188,7 @@ class Battle {
 					buf[n][m].turn--;
 					if(buf[n][m].turn <= 0) {
 						System.out.println(buf[n][m].name + "の効果が切れた！");
-						window.println(buf[n][m].name + "のこうかがきれた");
+						window.println(buf[n][m].name + "のこうかがきれた!");
 						buf[n][m] = null;//この後順列をきれいにする処理を
 					}
 				}
@@ -210,6 +210,8 @@ class Battle {
 			}
 		}
 	}
+
+	ArrayList<Integer> EMPTY = new ArrayList<>();
 
 	public void selecthero() {
 		ArrayList<String> cmd1 = new ArrayList<String>(Arrays.asList("もどる", "こうげき", "ぼうぎょ", "スキル"));
@@ -290,7 +292,7 @@ class Battle {
 						continue;
 					}
 
-					selects = ID.get(window.getOption(cmd2, BattleWindow.CMD_RIGHT_BOX8)) - 1;
+					selects = ID.get(window.getOption(cmd2, BattleWindow.CMD_RIGHT_BOX8, BattleWindow.SKILL, ID, EMPTY, EMPTY)) - 1;
 
 					if(selects == -1) {
 						n--;
@@ -337,11 +339,13 @@ class Battle {
 	public Item useItem(int k) {
 		ArrayList<String> cmd = new ArrayList<>();
 		ArrayList<Integer> ID = new ArrayList<>();
+		ArrayList<Integer> cnt = new ArrayList<>();
 		for(int n = 1; n < 24; n++) {
 			if(data.checkItem(n)) {
 				System.out.println(n  + ":"+ ItemData.getItemName(n) + "(" + data.itemCnt[n] + ")");
 				cmd.add(ItemData.getItemName(n) + " " + data.itemCnt[n] + "コ");
 				ID.add(n);
+				cnt.add(data.itemCnt[n]);
 			}
 		}
 
@@ -351,7 +355,7 @@ class Battle {
 			return null;
 		}
 
-		int m = ID.get(window.getOption(cmd, BattleWindow.CMD_RIGHT_BOX4));
+		int m = ID.get(window.getOption(cmd, BattleWindow.CMD_RIGHT_BOX4, BattleWindow.ITEM, EMPTY, ID, cnt));
 		itemFlag[k] = m;
 		return ItemData.getItem(m);
 	}
